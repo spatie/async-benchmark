08e82d8bcc5fcda1a4106a872514607931e91a40
Niklas Keller <me@kelunik.com>
2018-01-03 17:04:10 +0100

    Remove third benchmark
    The third benchmark only tested ReactPHP and Amp now instead of using
    any parallel API. The first benchmark does basically what the third
    benchmark did before.

--- src/benchmarks/1/amp.php
+++ src/benchmarks/1/amp.php
@@ -6,14 +6,16 @@ Amp\Parallel\Worker\pool(new Amp\Parallel\Worker\DefaultPool(16));
 
 $counter = 0;
 
-for ($i = 1; $i <= 50; $i++) {
-    $promises[] = Amp\ParallelFunctions\parallel(function () {
-        sleep(1);
-
+for ($i = 1; $i <= 500; $i++) {
+    $promise = Amp\ParallelFunctions\parallel(function () {
         return 2;
-    })->onResolve(function ($error, $output) use (&$counter) {
+    })();
+
+    $promise->onResolve(function ($error, $output) use (&$counter) {
         $counter += $output;
     });
+
+    $promises[] = $promise;
 }
 
 Amp\Promise\wait(Amp\Promise\all($promises));
--- src/benchmarks/1/spatie.php
+++ src/benchmarks/1/spatie.php
@@ -6,10 +6,8 @@ $pool = Spatie\Async\Pool::create();
 $pool->concurrency(16);
 $counter = 0;
 
-for ($i = 1; $i <= 50; $i++) {
+for ($i = 1; $i <= 500; $i++) {
     $pool[] = async(function () {
-        sleep(1);
-
         return 2;
     })->then(function ($output) use (&$counter) {
         $counter += $output;
